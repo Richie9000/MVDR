901e99898bba29104c956da13279040fea54a447
Ricardo <rr155914@gmail.com>
2021-05-01 08:46:42 -0500

    Added function door

--- src/game.ts
+++ src/game.ts
@@ -1,43 +1,32 @@
 import * as utils from '@dcl/ecs-scene-utils'
 
 
-
-
+function mvdr() {
 const baseScene = new Entity();
 engine.addEntity(baseScene);
 baseScene.addComponent(new GLTFShape("models/mvdr.glb"));
 baseScene.addComponent(new Transform({ position: new Vector3(10, 0, 20)}));
-// Add an entity for the door
+}
+mvdr();
+
+function door(){
 const door = new Entity(); 
 engine.addEntity(door);
 
-// Give it a model and move it into place
+
 door.addComponent(new GLTFShape("models/Door/door.glb"));
 door.addComponent(new Transform({ position: new Vector3(10, 0, 20) }));
-
-// Add an Animator to play clips inside the model file, created by the artist
-door.addComponent(new Animator());
-// This model has an "Open" animation that when played should happen once and then stop moving
-door
-  .getComponent(Animator)
-  .addClip(new AnimationState("Door_Open", { looping: false }));
-
-
-
-// When the player clicks on the door, open it!
+let StartPos = new Vector3(10, 0, 20);
+let EndPos = new Vector3(10, 2, 20);
 let isDoorOpen = false;
 door.addComponent(
   new OnClick((): void => {
-    // Track if the door has already been opened so we don't play the animation twice
     if (!isDoorOpen) {
       isDoorOpen = true;
+      door.addComponent(new utils.MoveTransformComponent(StartPos, EndPos, 2))
 
-      // Play the animation
-      door
-        .getComponent(Animator)
-        .getClip("Door_Open")
-        .play();
-    
     }
   })
 );
+}
+door();
